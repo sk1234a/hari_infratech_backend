2fdf2998c1deaec8e74cf91d7a57a20f57378865
PAWAN SAHU <pawansahu17112003@gmail.com>
2026-03-10 09:43:41 +0530

    hari_infratech

--- routes/db.js
+++ routes/db.js
@@ -1,24 +1,16 @@
 require("dotenv").config()
 
-const { Client } = require("pg")
+const { Pool } = require("pg")
 
-const db = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 })
 
 db.connect()
-.then(()=>{
-console.log("Supabase PostgreSQL Connected")
-})
-.catch((err)=>{
-console.log("Database Error:",err)
-})
+.then(() => console.log("Supabase PostgreSQL Connected"))
+.catch(err => console.log("Database Error:", err))
 
 module.exports = db
